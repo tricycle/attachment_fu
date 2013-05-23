50131a157c893d64bfc64afba5798a933aef0386
Jais Cheema <jais@trikeapps.com>
2013-05-24 08:43:50 +1000

    Remove tempfile naming override

--- init.rb
+++ init.rb
@@ -1,15 +1,15 @@
 require 'tempfile'
 
-Tempfile.class_eval do
-  # overwrite so tempfiles use the extension of the basename.  important for rmagick and image science
-  def make_tmpname(basename, n)
-    ext = nil
-    # the n parameter is optional in ruby 1.9 so accomodate for that
-    # http://apidock.com/ruby/Dir/Tmpname/make_tmpname
-    n = n.nil? ? "" : n.to_s
-    sprintf("%s%d-%s%s", basename.to_s.gsub(/\.\w+$/) { |s| ext = s; '' }, $$, n, ext)
-  end
-end
+# Tempfile.class_eval do
+#   # overwrite so tempfiles use the extension of the basename.  important for rmagick and image science
+#   def make_tmpname(basename, n)
+#     ext = nil
+#     # the n parameter is optional in ruby 1.9 so accomodate for that
+#     # http://apidock.com/ruby/Dir/Tmpname/make_tmpname
+#     n = n.nil? ? "" : n.to_s
+#     sprintf("%s%d-%s%s", basename.to_s.gsub(/\.\w+$/) { |s| ext = s; '' }, $$, n, ext)
+#   end
+# end
 
 require 'geometry'
 ActiveRecord::Base.send(:extend, Technoweenie::AttachmentFu::ActMethods)
